8569e9b11ceb965d97d1b2df3c2deb2eb069b01b
Denkotleta221 <trillpill09@gmail.com>
2025-08-27 17:57:54 +0200

    Change on Class

--- js/ITstep.js
+++ js/ITstep.js
@@ -1,6 +1,26 @@
-const ball = document.getElementById("ball");
+class Field {
+  constructor(container) {
+    this.element = document.createElement("div");
+    this.element.classList.add("field");
+    container.appendChild(this.element);
+  }
+}
 
-document.body.addEventListener("click", function(event) {
-  ball.style.left = (event.clientX - 50) + "px";
-  ball.style.top = (event.clientY - 50) + "px";
-});
+class Ball {
+  constructor(field, src) {
+    this.element = document.createElement("img");
+    this.element.classList.add("ball");
+    this.element.src = src;
+    field.element.appendChild(this.element);
+
+    const ball = this.element;
+
+    field.element.addEventListener("click", function(event) {
+      ball.style.left = (event.clientX - 50) + "px";
+      ball.style.top = (event.clientY - 50) + "px";
+    })
+  }
+}
+
+const field1 = new Field(document.body);
+const ball = new Ball(field1, "./img/ball.png");
